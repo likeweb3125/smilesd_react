b55b87047b59811e04309420b7ee0f40db70bd5b
sol772 <53934919+sol772@users.noreply.github.com>
2024-06-04 19:00:38 +0900

    api_uri 변경적용

--- src/config/enum.js
+++ src/config/enum.js
@@ -1,4 +1,5 @@
-const api_uri = "http://smilesdapi.likeweb.co.kr";
+const api_uri = "https://www.event-invitation.co.kr:446";
+
 
 exports.enum_api_uri = {
     api_uri: `${api_uri}`,
@@ -11,7 +12,5 @@ exports.enum_api_uri = {
     //이벤트
     event_list: `${api_uri}/v1/arko/eventList`,
     event_ex: `${api_uri}/v1/arko/eventListEx`,
-
-
 
 }
